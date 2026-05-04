b6e7d1f7ddb93e0c9474761b937ab84d992ee472
Rudra Patel <rudrapatel25249@gmail.com>
2026-05-04 12:12:44 +0530

    fix(web): ensure middleware can validate NextAuth session

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,8 +1,12 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/auth/sign-in",
+  },
+  callbacks: {
+    authorized: ({ token }) => !!token,
   },
 });
 
